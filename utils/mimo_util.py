6300ce8fb1d45c5715ff46a904c9d82fbeec120b
arianaolson419 <arianaolson419@gmail.com>
2019-04-16 10:19:07 -0400

    Create library for project functions

---
+++ utils/mimo_util.py
@@ -0,0 +1,20 @@
+"""This file contains functions used to imlement MIMO in hardware with space-time coding.
+"""
+
+from __future__ import division, print_function
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_symbols(num_symbols, num_channels, seed):
+    """Generate multiple channels of random complex data symbols
+
+    Args:
+        num_symbols (int): The number of symbols per channel.
+        num_channels (int): The number of channels of data.
+        seed (int): The seed used by the random number generator.
+
+    Returns:
+        symbols (complex ndarray): An array of size (num_channels, num_symbols)
+        symbols of +/-1 +/-1j.
+    """
+    pass
