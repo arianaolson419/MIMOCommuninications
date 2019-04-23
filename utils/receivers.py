4b2c5be7bc63a87f62d008dd50bf32f21d020237
kuannie1 <anne.ku4@gmail.com>
2019-04-23 09:57:20 -0400

    got the right file

--- utils/receivers.py
+++ utils/receivers.py
@@ -6,50 +6,14 @@ from __future__ import print_function, division
 import numpy as np
 import matplotlib.pyplot as plt
 
-def estimate_channel(rx1_header, rx2_header, zeros1, zeros2, tx1_header, tx2_header):
-    """Estimate the channel using the headers sent from each antenna.
+def detect_start_lts(signal_time_rx, lts, signal_length):
+    cross_corr = np.correlate(signal_time_rx, lts)
+    lag = np.argmax(np.abs(cross_corr)) - 1
 
-    Channel estimation is the same for both the zero-forcing and MMSE
-    receivers. This function uses copies of the arrays given to it because any
-    0s are replaced by small numbers ot avoid division errors.
+    return lag, signal_time_rx[lag:lag+signal_length]
 
-    Args:
-        rx1_header (complex 1D ndarray): The portion of the signal received at
-            rx antenna 1 corresponding to the header transmitted at tx antenna
-            1.
-        rx2_header (complex 1D ndarray): The portion of the signal received at
-            rx antenna 2 corresponding to the header transmitted at tx antenna
-            2.
-        zeros1 (complex 1D ndarray): The portion of the signal received at rx
-            antenna 1 corresponding the the header transmitted at tx antenna
-            2.
-        zeros2 (complex 1D ndarray): The portion of the signal received at rx
-            antenna 2 corresponding the the header transmitted at tx antenna
-            1.
-        tx1_header (complex 1D ndarray): The header transmitted from tx antenna
-            1.
-        tx2_header (complex 1D ndarray): The header transmitted from tx antenna
-            2.
 
-    Returns:
-        H (complex (2, 2) ndarray): A matrix of channel estimations.
-    """
-    header11 = np.copy(rx1_header)
-    header21 = np.copy(zeros2)
-    header12 = np.copy(zeros1)
-    header22 = np.copy(rx2_header)
 
-    # Replace 0s in denominators to avoid division errors.
-    tx1_header[tx1_header == 0] = 1e-12
-    tx2_header[tx2_header == 0] = 1e-12
-    
-    H = np.zeros((2, 2), dtype=np.complex64)
-    H[0][0] = np.mean(header11 / tx1_header)
-    H[0][1] = np.mean(header12 / tx2_header)
-    H[1][0] = np.mean(header21 / tx1_header)
-    H[1][1] = np.mean(header22 / tx2_header)
-
-    return H
 
 def estimate_channel_mimo(rx_sections, tx_headers):
     """Estimate the channel using the headers sent from each antenna.
@@ -78,6 +42,22 @@ def estimate_channel_mimo(rx_sections, tx_headers):
             H[i, j] = np.mean(rx_sections[i, j, :] / tx_headers[i, :rx_sections.shape[-1]])
 
     return H
+
+def estimate_channel_alamouti(rx_sections, tx_headers):
+    """Given 2 two transmit antennas & 1 receive, make a channel matrix like this:
+        [[h1, h2]
+        [h*_2, -h*_1]]
+    
+    Arguments:
+        rx_sections {[type]} -- [description]
+        tx_headers {[type]} -- [description]
+    """
+    # TODO: parse the tx and rx sections accordingly!
+    h1 = estimate_channel_mimo(rx_sections[], tx_headers[])
+    h2 = estimate_channel_mimo(rx_sections[], tx_headers[])
+    # TODO: return matrix from format in function's docstring
+    pass
+
 
 def calculate_weights_zero_forcing(H):
     """Calculates the weight matrix for the zero-forcing receiver.
@@ -115,6 +95,7 @@ def calculate_weights_mmse(tx_power, sigma, H):
     W = tx_power * np.linalg.inv(R) * H
 
     return W
+
 
 def recover_signals(rx1, rx2, W):
     """Estimates the sent signals using the weight matrix.
@@ -184,3 +165,83 @@ def recover_signals_csi(rx, U):
     """
     s_est = np.matmul(np.transpose(np.conjugate(U)), rx)
     return s_est
+
+def estimate_f_delta(lts, num_samples):
+    """Estimate the frequency offset of a received OFDM signal using the LTS.
+
+    Args:
+        lts (1D complex ndarray): A block of random complex values taking on
+            values of +-1 +-j of length 3 * num_samples that has been sent through a nonflat channel. This contains 3 repeating
+            blocks of size num_samples.
+        num_samples (int): The number of samples in each LTS block.
+
+    Returns:
+        f_delta_est (float): The average estimated frequency offset, in radians.
+    """
+    sum_f_delta_ests = 0
+    for i in range(num_samples):
+        complex_exp = lts[2 * num_samples + i] / lts[num_samples + i]
+        sum_f_delta_ests += np.angle(complex_exp)
+
+    return sum_f_delta_ests / (num_samples ** 2)
+
+
+
+def detect_start(signal_time_rx, header, signal_length):
+    """Find the start of the time domain signal using crosss correlation.
+
+    Args:
+        signal_time_rx (1D complex ndarray): A received time domain signal
+            that conatains a known header as the first portion of the signal.
+        header (1D complex ndarray): A known signal that is included at the
+            beginning of the transmitted signal.
+        signal_length (int): The length of the signal that whas transmitted.
+
+    Returns:
+        signal_time_start (1D complex ndarray): A version of signal_time_rx
+            that starts at the first data sample.
+    """
+    cross_corr = np.correlate(signal_time_rx, header)
+    lag = np.argmax(cross_corr) - 1
+
+    return signal_time_rx[lag:lag+signal_length]
+
+def correct_freq_offset(signal_time, f_delta):
+    """Correct for the frequency offset in a time-domain signal.
+
+    Args:
+        signal_time (1D complex ndarray): A time domain signal that starts with
+            the LTS used to estimate the frequency offset.
+        f_delta (float): The frequency offset to correct for, in radians.
+
+    Returns:
+        signal_time_corrected (1D complex ndarray): The corrected time domain
+            signal. Has the same shape as signal_time.
+    """
+    exponentials = np.exp(np.arange(signal_time.shape[-1]) * 1j * f_delta)
+    signal_time_corrected = signal_time / exponentials
+
+    return signal_time_corrected
+
+
+def decode_signal_freq(channel_coefficients, signal_freq):
+    """Decode a frequency domain signal into a series of bits.
+
+    Args:
+        signal_freq (1D float32 ndarray): The bit sequence in the frequency
+            domain.
+
+    Returns:
+        bits (1D ndarray): The bit sequence decoded from signal_freq.
+    """
+    # TODO: Find the symbol that's closest to symbol 0
+    # h1 = 
+    # h2 = 
+    # h1_conjugate = 
+    # h2_conjugate = 
+
+    bits = np.sign(signal_freq.real) + 1j * np.sign(signal_freq.imag)
+
+    return bits
+
+
