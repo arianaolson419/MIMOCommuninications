d38ce51b8f6fb1754f7bb246e2e9dfd614b65cd6
arianaolson419 <arianaolson419@gmail.com>
2019-05-07 12:50:27 -0400

    Clean up code

--- decode.py
+++ decode.py
@@ -1,31 +1,74 @@
 """This script decodes received MIMO data.
-"""
 
+Authors: Annie Ku, Ariana Olson
+"""
 import numpy as np
 import matplotlib.pyplot as plt
-import utils.receivers as receivers
 import utils.mimo_util as mimo
+import argparse
 
-# TODO: Update this when saving in encode.py is updated.
-tx_info = np.load('tx_info.npz')
+# Set up commandline arguments.
+FLAGS = None
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--received_data_path',
+                    type=str,
+                    default='Data/MIMOReceive.dat',
+                    help='The path of the binary file containing received data')
+parser.add_argument('--isolated_signal_path',
+                    type=str,
+                    default='isolated_signal.npz',
+                    help="""The path of a .npz file containing the isolated
+                    data signal obtained using cross correlation on the full
+                    received signal.""")
+parser.add_argument('--tx_info_path',
+                    type=str,
+                    default='tx_info.npz',
+                    help="""The path of the transmission information generated
+                    by encode.py.""")
+parser.add_argument('--figure_directory',
+                    type=str,
+                    default='figures',
+                    help='The directory to save images to')
+parser.add_argument('--use_saved_signal',
+                    action='store_true',
+                    default=False,
+                    help="""Use --isolated_signal_path to load a signal that
+                    has already been isolated from the received data.
+                    Otherwise, the program will perform cross correlation on
+                    the received data to isolate the signal, which can take
+                    several minutes.""")
+parser.add_argument('--make_plots',
+                    action='store_true',
+                    default=False,
+                    help="Generate and display all plots")
+
+FLAGS, unparsed = parser.parse_known_args()
+
+# Image saving parameters.
+save_name = FLAGS.figure_directory + '/{}.png'
+figsize_constellation = (10, 10)
+figsize_rect = (10, 5)
+
+# Load transmission data that was created in encode.py.
+tx_info = np.load(FLAGS.tx_info_path)
 header = tx_info['header']
 data = tx_info['data']
 tx_combined = tx_info['combined']
 symbols = tx_info['data_bits']
 
-
-use_saved_signal = False
-
-if not use_saved_signal:
-# load received data
-    received_data = np.fromfile("Data/MIMOReceive.dat", dtype=np.float32)
+if not FLAGS.use_saved_signal:
+    # load received data
+    received_data = np.fromfile(FLAGS.received_data_path, dtype=np.float32)
     signal_time_rx = received_data[::2] + received_data[1::2]*1j
 
     plt.plot(signal_time_rx.real)
     plt.show()
 
     signal_len = tx_combined.shape[-1]
-    lag, signal_time_rx = receivers.detect_start_lts(signal_time_rx, header[0], signal_len)
+    lag, signal_time_rx = mimo.detect_start(signal_time_rx,
+            header[0],
+            signal_len, plot=FLAGS.make_plots)
 
     plt.plot(signal_time_rx.real)
     plt.show()
@@ -33,79 +76,96 @@ if not use_saved_signal:
     np.savez('isolated_signal.npz', signal_rx=signal_time_rx)
 
 else:
-    signal_time_rx = np.load('isolated_signal.npz')["signal_rx"]
+    # Use a signal that has previously been isolated with cross correlation.
+    signal_time_rx = np.load(FLAGS.isolated_signal_path)["signal_rx"]
 
-rx_header_1 = signal_time_rx[:header.shape[-1]]
-rx_header_2 = signal_time_rx[header.shape[-1]+mimo.ZERO_SAMPLES:header.shape[-1] + mimo.ZERO_SAMPLES + header.shape[-1]]
+# Isolate the headers for timing offset correction and channel estimation.
+header_1_end = header.shape[-1]
+header_2_start = header.shape[-1]+mimo.ZERO_SAMPLES
+header_2_end = header_2_start + header.shape[-1]
+rx_header_1 = signal_time_rx[:header_1_end]
+rx_header_2 = signal_time_rx[header_2_start:header_2_end]
 
-# # # correct for timing differences using timing_offset/f_delta variable
-f_delta_header_2, y_scale_header_2 = receivers.find_f_delta(rx_header_2)
-
+# correct for timing differences using timing_offset/f_delta variable
+f_delta_header_2, y_scale_header_2 = mimo.find_f_delta(rx_header_2)
 print("f_delta from header 2: ", f_delta_header_2)
 
-# plotting constellation before timing correction
-plt.plot(signal_time_rx.real[10::20], signal_time_rx.imag[10::20], ".")
-plt.title("Received Signal Constellation Before Timing Offset")
-plt.xlabel("Real")
-plt.ylabel("Imaginary")
-plt.show()
+if FLAGS.make_plots:
+    # Plot constellation before timing correction
+    plt.figure(figsize=figsize_constellation)
+    plt.plot(signal_time_rx.real[10::20], signal_time_rx.imag[10::20], ".")
+    plt.title("Received Signal Constellation Before Timing Offset")
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.savefig(save_name.format('received_const_before_timing'))
+    plt.show()
 
-signal_time_rx = receivers.correct_timing_offset(f_delta_header_2, y_scale_header_2, signal_time_rx)
+# Correct for the timing offset.
+signal_time_rx = mimo.correct_timing_offset(f_delta_header_2,
+        y_scale_header_2,
+        signal_time_rx)
 
-plt.plot(signal_time_rx)
-plt.title("Received Signal After Timing Correction")
-plt.show()
+if FLAGS.make_plots:
+    plt.figure(figsize=figsize_rect)
+    plt.plot(signal_time_rx)
+    plt.title("Received Signal After Timing Correction")
+    plt.savefig(save_name.format('received_after_timing'))
+    plt.show()
 
+    # Plot constellation after timing correction.
+    plt.figure(figsize=figsize_constellation)
+    plt.plot(signal_time_rx.real[:header.shape[-1]],
+            signal_time_rx.imag[:header.shape[-1]], ".")
+    plt.title("Received Header 1 Constellation After Timing Offset")
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.savefig(save_name.format('recevied_const_after_timing'))
+    plt.show()
 
+    # Plot the constellation of header 2 after timing offset correction.
+    plt.figure(figsize=figsize_constellation)
+    plt.plot(signal_time_rx.real[header_2_start:header_2_end],
+            signal_time_rx.imag[header_2_start:header_2_end], ".")
+    plt.title("Received Header 2 Constellation After Timing Offset")
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.savefig(save_name.format('received_h2_after_timing'))
+    plt.show()
 
-# plotting constellation after timing correction
-plt.plot(signal_time_rx.real[:header.shape[-1]], signal_time_rx.imag[:header.shape[-1]], ".")
-plt.title("Received Header 1 Constellation After Timing Offset")
-plt.xlabel("Real")
-plt.ylabel("Imaginary")
-plt.show()
+# Estimate the channel.
+H = mimo.estimate_channel_alamouti(rx_header_1, rx_header_2, header[0], header[1])
+print("channel matrix H:\n{}".format(H))
 
-plt.plot(signal_time_rx.real[header.shape[-1]+mimo.ZERO_SAMPLES:header.shape[-1] + mimo.ZERO_SAMPLES + header.shape[-1]], signal_time_rx.imag[header.shape[-1]+mimo.ZERO_SAMPLES:header.shape[-1] + mimo.ZERO_SAMPLES + header.shape[-1]], ".")
-plt.title("Received Header 2 Constellation After Timing Offset")
-plt.xlabel("Real")
-plt.ylabel("Imaginary")
-plt.show()
+data_start = header_2_end + mimo.ZERO_SAMPLES
+data_end = data_start + data.shape[-1]
+rx_data = signal_time_rx[data_start:data_end]
 
+# Downsample to account for Alamouti encoding.
+downsample_start = mimo.SYMBOL_PERIOD // 2
+downsample_n_samples = mimo.SYMBOL_PERIOD
+rx_data_downsampled = rx_data[downsample_start::downsample_n_samples]
 
-rx_header_1 = signal_time_rx[:header.shape[-1]]
-rx_header_2 = signal_time_rx[header.shape[-1]+mimo.ZERO_SAMPLES:header.shape[-1] + mimo.ZERO_SAMPLES + header.shape[-1]]
+# Correct for the channel and recover the signal.
+recovered_signal = mimo.recover_signals_alamouti(rx_data_downsampled, H) 
 
+if FLAGS.make_plots:
+    # Plot the constellation of the recovered signal.
+    plt.figure(figsize=figsize_constellation)
+    plt.plot(recovered_signal.real, recovered_signal.imag, ".")
+    plt.title("Recovered Signal Constellation")
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.savefig(save_name.format('recovered_const'))
+    plt.show()
 
+recovered_signal_symbols = mimo.turn_data_to_bits(recovered_signal)
 
-# estimate the channel
-H = receivers.estimate_channel_alamouti(rx_header_1, rx_header_2, header[0], header[1])
+if FLAGS.make_plots:
+    # Plot per sample if the recovered bits equal the bits transmitted.
+    plt.figure(figsize=figsize_rect)
+    plt.plot(recovered_signal_symbols == symbols)
+    plt.title("Per Sample Error")
+    plt.savefig(save_name.format('errors'))
+    plt.show()
 
-print("H:\n", H)
-
-rx_data = signal_time_rx[header.shape[-1] + mimo.ZERO_SAMPLES + header.shape[-1] + mimo.ZERO_SAMPLES: header.shape[-1] + mimo.ZERO_SAMPLES + header.shape[-1] + mimo.ZERO_SAMPLES + data.shape[-1]]
-
-rx_data_downsampled = rx_data[10::20]
-
-
-recovered_signal = receivers.recover_signals_alamouti(rx_data_downsampled, H) 
-
-
-plt.plot(recovered_signal.real, recovered_signal.imag, ".")
-plt.title("Recovered Signal Constellation")
-plt.xlabel("Real")
-plt.ylabel("Imaginary")
-plt.show()
-
-
-
-
-recovered_signal_symbols = receivers.turn_data_to_bits(recovered_signal)
-
-# plt.plot(recovered_signal_bits == tx_qpsk_bits)
-# plt.show()
-print("error rate (no rotation): ", receivers.calculate_error_rate(symbols, recovered_signal_symbols))
-
-
-# for n in range(8):
-#     rotated_recovered_signal_symbols = receivers.turn_data_to_bits(recovered_signal * np.exp((np.pi/4) * n * 1j))
-#     print("error rate: ", receivers.calculate_error_rate(symbols, rotated_recovered_signal_symbols))
+print("Error rate: {}".format(mimo.calculate_error_rate(symbols, recovered_signal_symbols)))
--- utils/mimo_util.py
+++ utils/mimo_util.py
@@ -19,12 +19,11 @@ def generate_symbols(num_symbols, seed):
 
     Args:
         num_symbols (int): The number of symbols per channel.
-        num_channels (int): The number of channels of data.
         seed (int): The seed used by the random number generator.
 
     Returns:
-        symbols (complex 1D ndarray): An array of size (num_channels, num_symbols)
-            symbols of +/-1 +/-1j.
+        symbols (complex 1D ndarray): An array of size (num_symbols,) symbols of
+        +/-1 +/-1j.
     """
     all_symbols = np.zeros((num_symbols))
     np.random.seed(seed)
@@ -36,16 +35,18 @@ def generate_symbols(num_symbols, seed):
 
 def generate_header_data(symbols, symbol_period):
     """Generate data from an array of complex symbols to transmit over USRP.
-    Symbols are modulated with rectangular pulses. This data is not Alamouti
+    Symbols are modulated with rectangular pulses. This data is not alamouti
     encoded.
 
     Args:
-        symbols (complex 1D ndarray): An array of length num_symbols
-            symbols of +/-1 and +/-1j.
+        symbols (complex 1d ndarray): An array of symbols of values +/-1 and
+            +/-1j.
         symbol_period (int): The number of samples in a single pulse.
 
     Returns:
-        header_data (1D complex ndarray): An array representing the data of one header.
+        header_data (1d complex ndarray): An array of
+            shape(symbols.shape[-1] * symbol_period,) representing the data of
+            one header.
     """
     pulse = np.ones(symbol_period)
     x = np.zeros((symbols.shape[-1] * symbol_period), dtype=np.complex128)
@@ -55,19 +56,20 @@ def generate_header_data(symbols, symbol_period):
     return header
 
 def generate_tx_data_2x2(symbols, symbol_period):
-    """Generate alamouti encoded data from an array of complex symbols to
+    """Generate Alamouti encoded QPSK data from an array of complex symbols to
     transmit over USRP. Symbols are modulated with rectangular pulses.
 
     Args:
-        symbols (complex 1D ndarray): An array of length num_symbols
-            symbols of +/-1 and +/-1j.
+        symbols (complex 1d ndarray): An array of symbols of values +/-1 and
+            +/-1j.
         symbol_period (int): The number of samples in a single pulse.
 
     Returns:
-        tx_1 (1D complex ndarray): An array representing the data to transmit
-            from the first antenna
-        tx_2 (1D complex ndarray): An array representing the data to transmit
-            from the second antenna
+        tx (2D complex ndarray): An array of shape
+            (2, symbols.shpae[-1] * symbol_period) representing the data to
+            transmit from both antennas. The first row contains data to transmit
+            from the first antenna and the second row contains data to transmit
+            from the second antenna.
     """
 
     # Encode the symbols with the Alamouti scheme.
@@ -78,6 +80,7 @@ def generate_tx_data_2x2(symbols, symbol_period):
     alamouti[0, 1::2] = -np.conj(symbols[1::2])
     alamouti[1, 1::2] = np.conj(symbols[::2])
 
+    # Modulate for QPSK.
     pulse = np.ones(symbol_period)
     tx1 = np.zeros((2, alamouti.shape[-1] * symbol_period), dtype=np.complex128)
     tx1[0, ::symbol_period] = alamouti[0].real
@@ -87,12 +90,11 @@ def generate_tx_data_2x2(symbols, symbol_period):
     tx2[0, ::symbol_period] = alamouti[1].real
     tx2[1, ::symbol_period] = alamouti[1].imag
 
-    # Modulate for QPSK.
-    tx1[0] = np.convolve(tx1[0], pulse)[:alamouti.shape[-1] * symbol_period]    # Real
-    tx1[1] = np.convolve(tx1[1], pulse)[:alamouti.shape[-1] * symbol_period]    # Imag
+    tx1[0] = np.convolve(tx1[0], pulse)[:alamouti.shape[-1] * symbol_period]    # real
+    tx1[1] = np.convolve(tx1[1], pulse)[:alamouti.shape[-1] * symbol_period]    # imag
 
-    tx2[0] = np.convolve(tx2[0], pulse)[:alamouti.shape[-1] * symbol_period]    # Real
-    tx2[1] = np.convolve(tx2[1], pulse)[:alamouti.shape[-1] * symbol_period]    # Imag
+    tx2[0] = np.convolve(tx2[0], pulse)[:alamouti.shape[-1] * symbol_period]    # real
+    tx2[1] = np.convolve(tx2[1], pulse)[:alamouti.shape[-1] * symbol_period]    # imag
 
     tx = np.zeros((2, tx1.shape[-1]), dtype=np.complex128)
 
@@ -107,7 +109,7 @@ def generate_tx_data_2x2(symbols, symbol_period):
 def add_headers_2x2(tx_data, headers, zero_samples, scale=1.0):
     """Combine headers and data for transmission.
 
-    The order of transmission is as follows:
+    the order of transmission is as follows:
     1. Both antennas transmit zero_samples number of zeros.
     2. Antenna 1 transmits header_1 and antenna 2 transmits the same number of
         zeros.
@@ -115,7 +117,7 @@ def add_headers_2x2(tx_data, headers, zero_samples, scale=1.0):
     4. Antenna 2 transmits header_2 and antenna 1 transmits the same number of
         zeros
     5. Both antennas transmit zero_samples number of zeros.
-    6. Antenna 1 transmits tx_1 and Antenna 2 transmits tx_2.
+    6. Antenna 1 transmits tx_1 and antenna 2 transmits tx_2.
 
     Args:
         tx_data (complex ndarray): A two-row array with each row representing
@@ -149,10 +151,10 @@ def add_headers_2x2(tx_data, headers, zero_samples, scale=1.0):
     return tx_combined * scale
 
 def interleave_and_save_data(tx_combined, dest_path_tx1, dest_path_tx2):
-    """Interleave the real and imaginary components of the transmission sequences
+    """Create data files to transmit over USRP.
 
     Args:
-        tx_combined (complex ndarray): A two-row array containing the full
+        tx_combined (complex 2D ndarray): A two-row array containing the full
             transmission sequence for each antenna, including headers. Each row
             represents the sequence for one antenna.
         dest_path_tx_1 (string): The path to save the sequence for antenna 1 to.
@@ -164,3 +166,221 @@ def interleave_and_save_data(tx_combined, dest_path_tx1, dest_path_tx2):
 
     tmp[0, :].tofile(dest_path_tx1)
     tmp[1, :].tofile(dest_path_tx2)
+
+def detect_start(signal_time_rx, header, signal_length, plot=False):
+    """Detect the start of the signal from received data using cross correlation.
+
+    Args:
+        signal_time_rx (complex 1D ndarray): A time domain signal of receieved
+            data containing the signal.
+        header (complex 1D ndarray): The header to use for cross correlation.
+        signal_length (int): The number of samples in the signal to isolate.
+        plot (bool): If True, plot the cross correlation of the signal and the
+            header. Default is False.
+
+    Returns:
+       lag (int): The index of the first sample of the signal in the received
+           data.
+       signal_time_rx (complex 1D ndarray): The time domain signal isolated
+           from the received data. Its shape is (signal_len,)
+    """
+    cross_corr = np.correlate(signal_time_rx, header)
+    lag = np.argmax(np.abs(cross_corr)) - 1
+
+    if plot:
+        plt.plot(cross_corr)
+        plt.title("corr")
+        plt.show()
+
+    print('start of signal: {}'.format(lag))
+
+    return lag, signal_time_rx[lag:lag+signal_length]
+
+def estimate_channel(rx_header_1, rx_header_2, tx_header_1, tx_header_2):
+    """Estimate the channel using the headers sent from each antenna.
+    Channel estimation is the same for both the zero-forcing and MMSE
+    receivers. This function uses copies of the arrays given to it because any
+    0s are replaced by small numbers ot avoid division errors.
+    Args:
+        rx_header_1 (complex 1D ndarray): The portion of the signal received at
+            rx antenna 1 corresponding to the header transmitted at tx antenna
+            1.
+        rx_header_2 (complex 1D ndarray): The portion of the signal received at
+            rx antenna 2 corresponding to the header transmitted at tx antenna
+            2.
+        tx_header_1 (complex 1D ndarray): The header transmitted from tx antenna
+            1.
+        tx_header_2 (complex 1D ndarray): The header transmitted from tx antenna
+            2.
+    Returns:
+        H (complex (2,) ndarray): A matrix of channel estimations.
+    """
+    header11 = np.copy(rx_header_1)
+    header12 = np.copy(rx_header_2)
+
+    # Replace 0s in denominators to avoid division errors.
+    tx_header_1[tx_header_1 == 0] = 1e-12
+    tx_header_2[tx_header_2 == 0] = 1e-12
+
+    H = np.zeros(2, dtype=np.complex128)
+    H[0] = np.mean(header11 / tx_header_1)
+    H[1] = np.mean(header12 / tx_header_2)
+
+    return H
+
+def estimate_channel_alamouti(rx_header_1, rx_header_2, tx_header_1, tx_header_2):
+    """Estimate the channel for a 2X1 MIMO system with Alamouti encoding. The channel estimation matrix is as follows:
+       H = [[h1, h2]
+           [h*_2, -h*_1]]
+    Where h1 is the channel from the first transmit antenna to the receive
+    antenna, and h2 is the channel from the second transmit antenna to the
+    receive antenna.
+    
+    Args:
+        rx_header_1 (complex 1D ndarray): The header received from the first
+            antenna.
+        rx_header_2 (complex 1D ndarray): The header received from the second
+            antenna.
+        tx_header_1 (complex 1D ndarray): The header transmitted from the first
+            antenna.
+        tx_header_2 (complex 1D ndarray): The header transmitted from the
+            second antenna.
+    """
+    H = estimate_channel(rx_header_1, rx_header_2, tx_header_1, tx_header_2)
+    H_alamouti = np.zeros((2, 2), dtype=np.complex128)
+    H_alamouti[0][0] = H[0]
+    H_alamouti[0][1] = H[1]
+    H_alamouti[1][0] = np.conj(H[1])
+    H_alamouti[1][1] = -np.conj(H[0])
+
+    return H_alamouti
+
+def split_signal(ys):
+    """Split a one-dimensional signal into two rows in which each column
+    contains a symbol transmitted at one time and a signal transmitted at the
+    subsequent time for Alamouti decoding.
+    
+    Args:
+        ys (1D ndarray): An array of samples to split into two rows of data.
+    
+    Returns:
+        split (2D ndarray): A two-row array in which the first row contains
+            every other sample of ys starting at the first sample and the
+            second row contains the remaining samples.
+    """
+    split = np.zeros((2, ys.shape[-1] // 2), dtype=np.complex128)
+    split[0] = ys[::2]
+    split[1] = ys[1::2]
+    return split
+
+def merge_signal(split):
+    """Undoes the split_signal function.
+
+    For a 1D array ys of shape (1, N), N % 2 = 0,
+    merge_signal(split_signal(ys)) = ys.
+
+    Args:
+        split (2D ndarray): A two row array to merge into a one-dimensional
+            array.
+
+    Returns:
+        merge (1D ndarrys): A one dimensional array containing interleaved
+            samples of the rows of split, starting with split[0][0].
+    """
+    merge = np.zeros(split.shape[-1] * 2, dtype=np.complex128)
+    merge[::2] = split[0]
+    merge[1::2] = split[1]
+    return merge
+
+def recover_signals_alamouti(rx_data, H):
+    """Recovers an Alamouti encoded signal using a zero-forcing receiver.
+
+    Args:
+        rx_data (complex 1D ndarray): A downsampled Alamouti encoded signal to
+            recover.
+        H (complex (2, 2) ndarray): An array of channel estimations.
+
+    Returns:
+        recovered_merged (complex 1D ndarray): The recovered signal.
+    """
+    rx_split = split_signal(rx_data)
+    rx_split[1] = np.conj(rx_split[1])
+    
+    recovered = np.matmul(np.linalg.pinv(H), rx_split)
+    
+    recovered_merged = merge_signal(recovered)
+    
+    return recovered_merged
+
+def calculate_error_rate(recovered_signal_bits, transmitted_signal_bits):
+    """decode a frequency domain signal into a series of bits.
+
+    args:
+        signal_freq (1d float32 ndarray): the bit sequence in the frequency
+            domain.
+
+    returns:
+        bits (1d ndarray): the bit sequence decoded from signal_freq.
+    """
+    
+    return np.sum(recovered_signal_bits != transmitted_signal_bits)/recovered_signal_bits.shape[-1]
+
+def turn_data_to_bits(recovered_symbols):
+    """Use the sign of recovered samples to create symbols of +/-1 +/-1j for
+    bit error rate calculations.
+
+    Args:
+        recovered_symbols (complex 1D ndarray): An array of complex samples.
+
+    Returns:
+        symbols (complex 1D ndarray): An array of the same shape as
+            recovered_symbols containing values of +/-1 +/-1j.
+    """
+    symbols = np.sign(recovered_symbols.real) + 1j * np.sign(recovered_symbols.imag)
+    return symbols
+
+def find_f_delta(header):
+    """Estimate the frequency offset of a received QPSK signal.
+
+    Args:
+        header (complex 1D ndarray): A non-Alamouti encoded QPSK header to use
+            to estimate the frequency offset.
+    Returns:
+        f_delta (complex float): The frequecy offset of the received signal.
+        y_scale (complex_float): The magnitude of the maximum value of the fft.
+            This is used for correcting for the timing offset.
+    """
+    # Normalize.
+    h_mag_est = np.sqrt(np.mean(np.square(header)))
+    header_normalized = header / h_mag_est
+
+    # Create s[k] by raising the signal to the 4th power.
+    s = np.power(header_normalized, 4)
+    fft = np.fft.fft(s)
+    shifted_fft = np.fft.fftshift(fft)
+    freq_axis = np.linspace(-np.pi, np.pi - ((2 * np.pi)/(shifted_fft.shape[-1]+1)), shifted_fft.shape[-1])
+
+    # Get f_delta and theta from finding frequency value and peak height.
+    x_offset = freq_axis[np.argmax(np.abs(shifted_fft))]
+    y_height = np.max(shifted_fft)
+    y_scale = y_height / np.abs(y_height)
+    f_delta  = (x_offset) / -4 
+
+    return f_delta, y_scale
+
+def correct_timing_offset(f_delta, y_scale, signal_rx_time):
+    """Correct for timing offsets in a received signal.
+    
+    Args:
+        f_delta (complex float): The estimated frequency offset.
+        signal_rx_time (complex 1D ndarray): The entire time domain signal, including
+        both headers and data.
+
+    Returns:
+        x_est (complex 1D ndarray): The timing-offset corrected signal of the
+            same shape as signal_rx_time.
+    """
+    psi = f_delta * np.arange(0,signal_rx_time.shape[-1])
+    x_est = signal_rx_time * np.exp(1j * psi) / np.power(y_scale, 0.25)
+
+    return x_est
